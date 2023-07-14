bc3d4d790c1edd066bf6295a82aeb8d1d7597395
Rodolfo <rodolfo.machado@cilia.com.br>
2023-07-14 15:03:42 -0300

    Finalização do CRUD, feito de forma manual para admin/user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #dashboard
-    resources :admins, only: [:index, :edit, :update] #Administradores
+    resources :admins #Administradores
   end
 
   devise_for :users
