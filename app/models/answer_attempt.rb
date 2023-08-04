b4146bd4414aadaebf09385490bd0453de4bf428
Rodolfo <rodolfo.machado@cilia.com.br>
2023-08-04 12:42:26 -0300

    Ajustes para contadores, hide de questions e views de questions concluído

--- app/models/answer_attempt.rb
+++ app/models/answer_attempt.rb
@@ -2,4 +2,6 @@ class AnswerAttempt < ApplicationRecord
   belongs_to :user
   belongs_to :question
   belongs_to :answer
+
+  validates_uniqueness_of :user_id, scope: :question_id, message: 'Você já respondeu essa pergunta.'
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :subject, counter_cache: true, inverse_of: :questions
 
   has_many :answers, dependent: :destroy
+  has_many :answer_attempts, dependent: :destroy
 
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
@@ -12,6 +13,15 @@ class Question < ApplicationRecord
   paginates_per 5
 
   # Scopes
+  scope :answered_by_user, -> (user) {
+    joins(:answer_attempts).where('answer_attempts.user_id = ?', user.id)
+  }
+  scope :not_answered_by_user, -> (user) {
+    joins("LEFT JOIN answer_attempts ON (answer_attempts.question_id = questions.id AND answer_attempts.user_id = #{user.id})").
+      where('answer_attempts.id IS NULL').
+      group('questions.id')
+  }
+
   scope :_search_subject_, ->(page, subject_id) {
     includes(:answers, :subject).where(subject_id: subject_id).page(page)
   }
@@ -23,6 +33,14 @@ class Question < ApplicationRecord
   scope :last_questions, ->(page) {
     includes(:answers).order('RANDOM()').page(page)
   }
+
+  def self.number_of_questions_anwered_by_user(user)
+    answered_by_user(user).length
+  end
+
+  def self.number_of_questions_not_anwered_by_user(user)
+    not_answered_by_user(user).length
+  end
 
   private
 
